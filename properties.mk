11d2bd9bd2d262294e68dc0e65d2c971d58172d7
Martin Ponce <martin.ponce@outlook.com>
2020-01-11 12:56:05 +0800

    Personal customisation

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
 DEVICE = fenix5s
-SDK_HOME = /Users/daniel.siwiec/connectiq-sdk-mac-3.0.4
+SDK_HOME = /Users/marty/Documents/dev/sdk/connect-iq/connectiq-sdk-mac-3.1.6-2019-10-23-2de4665c6
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
-PRIVATE_KEY = /Users/daniel.siwiec/.ssh/id_rsa_garmin.der
+PRIVATE_KEY = /Users/marty/Documents/dev/sdk/connect-iq/dev-key/connect-iq-martin-ponce.key
